49ab474446fbd040988a52063806dc7c70956382
Rishabh Tayal <rtayal11@gmail.com>
2017-09-07 16:33:52 -0500

    initial commit

---
+++ server.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'spaceship'
+require 'json'
+
+get '/' do
+	Spaceship::Tunes.login("contact@appikon.com", "AppikonSoft121")
+	all_apps = Spaceship::Tunes::Application.all
+	all_apps.collect { |a|
+		{
+			name: a.name,
+		}
+	}.to_json
+end
+
+get '/ratings' do
+	Spaceship::Tunes.login("contact@appikon.com", "AppikonSoft121")
+	ratings = Spaceship::Tunes::Application.find('com.rtayal.ChatApp').ratings
+	# ratings.ratings.reviews("US").to_json
+	# ratings.collect { |r|
+	# 	{
+	# 		reviews: r.reviews("US")
+	# 	}
+	# }.to_json
+	ratings.reviews("US").collect { |review|
+		{
+			review: review.review,
+			rating: review.rating
+		}
+	}.to_json
+	# ratings.to_json
+end
