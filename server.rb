182b396f7ebb17a73133d045d262bf6fc59d6086
Rishabh Tayal <rtayal11@gmail.com>
2017-09-26 15:53:53 -0500

    commit

--- server.rb
+++ server.rb
@@ -26,9 +26,6 @@ get '/apps' do
 	username = params[:username]
 	password = params[:password]
 	Spaceship::Tunes.login(username, password)
-	Spaceship::Tunes.client.team_id = params[:team_id]
-	Spaceship::Portal.login(username, password)
-	Spaceship::Portal.client.team_id = params[:team_id]
 	all_apps = Spaceship::Tunes::Application.all
 	live_apps = all_apps.select { |app|
 		# app.live_version != nil
@@ -57,9 +54,6 @@ get '/ratings' do
 		store_front = ""
 	end
 	Spaceship::Tunes.login(username, password)
-	Spaceship::Tunes.client.team_id = params[:team_id]
-	Spaceship::Portal.login(username, password)
-	Spaceship::Portal.client.team_id = params[:team_id]
 	ratings = Spaceship::Tunes::Application.find(bundle_id).ratings
 	ratings.reviews(store_front).collect { |review|
 		{
@@ -90,9 +84,6 @@ post '/response' do
 	bundle_id = params[:bundle_id]
 	response = params[:response_text]
 	Spaceship::Tunes.login(username, password)
-	Spaceship::Tunes.client.team_id = params[:team_id]
-	Spaceship::Portal.login(username, password)
-	Spaceship::Portal.client.team_id = params[:team_id]
 	app = Spaceship::Tunes::Application.find(bundle_id)
 	body app.ratings.sendResponse(rating_id, response).to_json
 end
@@ -106,9 +97,6 @@ delete '/response' do
 	bundle_id = params[:bundle_id]
 	response_id = params[:response_id]
 	Spaceship::Tunes.login(username, password)
-	Spaceship::Tunes.client.team_id = params[:team_id]
-	Spaceship::Portal.login(username, password)
-	Spaceship::Portal.client.team_id = params[:team_id]
 	app = Spaceship::Tunes::Application.find(bundle_id)
 	body app.ratings.deleteResponse(rating_id, response_id).to_json
 end
@@ -119,9 +107,6 @@ get '/app_status' do
 	password = params[:password]
 	bundle_id = params[:bundle_id]
 	Spaceship::Tunes.login(username, password)
-	Spaceship::Tunes.client.team_id = params[:team_id]
-	Spaceship::Portal.login(username, password)
-	Spaceship::Portal.client.team_id = params[:team_id]
 	app = Spaceship::Tunes::Application.find(bundle_id)
 	app.edit_version.to_json
 end
