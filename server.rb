44f3ac0000f90104ad17edc4a5953bab3f0413ff
Eliott hauteclair <me@eliott.be>
2018-01-02 10:47:12 +0100

    Fix 500

--- server.rb
+++ server.rb
@@ -30,6 +30,8 @@ get '/app/metadata' do
     client = Spaceship::Tunes.login(username, password)
     client.team_id = '271949'
     
+    Spaceship::Tunes::Application.find(bundle_id)
+    
     version = app.live_version.version
     copyright = app.live_version.copyright
     status = app.live_version.app_status
