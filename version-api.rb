b9d0ea46c6fedbb8ee58abf3c9364e4ef539c425
Rishabh Tayal <rtayal11@gmail.com>
2018-01-05 13:48:47 -0600

    Version Bump

--- version-api.rb
+++ version-api.rb
@@ -6,12 +6,12 @@ get '/is_latest_api_version' do
   content_type :json
   file = File.read('version.json')
 version_json = JSON.parse(file)
-version = version_json
+version = version_json["version"]
 
   uri = URI('https://api.github.com/repos/RishabhTayal/itc-api/releases/latest')
   response = Net::HTTP.get(uri)
   json = JSON.parse(response)
-  if json["tag_name"] == version["version"]
+  if json["tag_name"] == version
   	return true.to_json
   else
   	return false.to_json
